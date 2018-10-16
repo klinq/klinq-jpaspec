e28bbb32ae0ed3d9ef67bc6150d296f0777b54f1
Simon Bahr <simon.bahr@storecast.de>
2018-10-16 10:05:52 +0200

    added possibility to distinct query results

--- src/main/kotlin/com/github/klinq/jpaspec/KlinqJpaSpec.kt
+++ src/main/kotlin/com/github/klinq/jpaspec/KlinqJpaSpec.kt
@@ -5,12 +5,16 @@ import javax.persistence.criteria.*
 import kotlin.reflect.KProperty1
 
 // Version of Specification.where that makes the CriteriaBuilder implicit
-private fun <T> where(makePredicate: CriteriaBuilder.(Root<T>) -> Predicate): Specification<T> =
-        Specification.where<T> { root, _, criteriaBuilder -> criteriaBuilder.makePredicate(root) }
+private fun <T> where(distinct: Boolean = false, makePredicate: CriteriaBuilder.(Root<T>) -> Predicate): Specification<T> =
+        Specification.where<T> { root, criteriaQuery, criteriaBuilder ->
+            criteriaQuery.distinct(distinct)
+            criteriaBuilder.makePredicate(root)
+
+        }
 
 class WhereBuilder<T, R>(private val path: (Root<T>) -> Path<R>) {
-    fun spec(makePredicate: CriteriaBuilder.(Path<R>) -> Predicate): Specification<T> =
-            where { root -> makePredicate(path(root)) }
+    fun spec(distinct: Boolean = false, makePredicate: CriteriaBuilder.(Path<R>) -> Predicate): Specification<T> =
+            where(distinct) { root -> makePredicate(path(root)) }
 
 }
 
@@ -40,29 +44,30 @@ fun <Z, R> KProperty1<in Z, Collection<R>>.toCollectionLeftJoin(): FromBuilder<Z
 // Equality
 fun <T, R> KProperty1<in T, R?>.equal(x: R): Specification<T> = toWhere().equal(x)
 
-fun <T, R> WhereBuilder<T, R?>.equal(x: R): Specification<T> = spec { equal(it, x) }
+fun <T, R> WhereBuilder<T, R?>.equal(x: R, distinct: Boolean = false): Specification<T> = spec(distinct) { equal(it, x) }
 
 fun <T, R> KProperty1<in T, R?>.notEqual(x: R): Specification<T> = toWhere().notEqual(x)
-fun <T, R> WhereBuilder<T, R?>.notEqual(x: R): Specification<T> = spec { notEqual(it, x) }
+fun <T, R> WhereBuilder<T, R?>.notEqual(x: R, distinct: Boolean = false): Specification<T> = spec(distinct) { notEqual(it, x) }
 
 //In
 fun <T, R> KProperty1<in T, R?>.`in`(values: Collection<R>): Specification<T> = toWhere().`in`(values)
 
-fun <T, R> WhereBuilder<T, R?>.`in`(values: Collection<R>): Specification<T> = createIn(values) { it }
+fun <T, R> WhereBuilder<T, R?>.`in`(values: Collection<R>, distinct: Boolean = false): Specification<T> = createIn(values, { it }, distinct)
 
 fun <T, R> KProperty1<in T, R?>.notIn(values: Collection<R>): Specification<T> = toWhere().notIn(values)
-fun <T, R> WhereBuilder<T, R?>.notIn(values: Collection<R>): Specification<T> = createIn(values) { it.not() }
+fun <T, R> WhereBuilder<T, R?>.notIn(values: Collection<R>, distinct: Boolean = false): Specification<T> = createIn(values, { it.not() }, distinct)
 
-private fun <R, T> WhereBuilder<T, R?>.createIn(values: Collection<R>, post: (Predicate) -> Predicate): Specification<T> =
+private fun <R, T> WhereBuilder<T, R?>.createIn(values: Collection<R>, post: (Predicate) -> Predicate, distinct: Boolean = false): Specification<T> =
         if (values.isNotEmpty()) {
-            spec { path ->
+            spec(distinct) { path ->
                 `in`(path)
                         .also { value -> values.forEach { value.value(it) } }
                         .let { post(it) }
             }
         } else {
             //SQL cannot handle empty in queries (at least some DBs), so default to false
-            Specification { _, _, criteriaBuilder ->
+            Specification { _, criteriaQuery, criteriaBuilder ->
+                criteriaQuery.distinct(distinct)
                 org.hibernate.query.criteria.internal.predicate.BooleanStaticAssertionPredicate(
                         criteriaBuilder as org.hibernate.query.criteria.internal.CriteriaBuilderImpl?, false)
             }
@@ -71,78 +76,78 @@ private fun <R, T> WhereBuilder<T, R?>.createIn(values: Collection<R>, post: (Pr
 // Comparison
 fun <T> KProperty1<in T, Number?>.le(x: Number) = toWhere().le(x)
 
-fun <T> WhereBuilder<T, Number?>.le(x: Number) = spec { le(it, x) }
+fun <T> WhereBuilder<T, Number?>.le(x: Number, distinct: Boolean = false) = spec(distinct) { le(it, x) }
 
 fun <T> KProperty1<in T, Number?>.lt(x: Number) = toWhere().lt(x)
-fun <T> WhereBuilder<T, Number?>.lt(x: Number) = spec { lt(it, x) }
+fun <T> WhereBuilder<T, Number?>.lt(x: Number, distinct: Boolean = false) = spec(distinct) { lt(it, x) }
 
 fun <T> KProperty1<in T, Number?>.ge(x: Number) = toWhere().ge(x)
-fun <T> WhereBuilder<T, Number?>.ge(x: Number) = spec { ge(it, x) }
+fun <T> WhereBuilder<T, Number?>.ge(x: Number, distinct: Boolean = false) = spec(distinct) { ge(it, x) }
 
 fun <T> KProperty1<in T, Number?>.gt(x: Number) = toWhere().gt(x)
-fun <T> WhereBuilder<T, Number?>.gt(x: Number) = spec { gt(it, x) }
+fun <T> WhereBuilder<T, Number?>.gt(x: Number, distinct: Boolean = false) = spec(distinct) { gt(it, x) }
 
 fun <T, R : Comparable<R>> KProperty1<in T, R?>.lessThan(x: R) = toWhere().lessThan(x)
-fun <T, R : Comparable<R>> WhereBuilder<T, R?>.lessThan(x: R) = spec { lessThan<R>(it, x) }
+fun <T, R : Comparable<R>> WhereBuilder<T, R?>.lessThan(x: R, distinct: Boolean = false) = spec(distinct) { lessThan<R>(it, x) }
 
 fun <T, R : Comparable<R>> KProperty1<in T, R?>.lessThanOrEqualTo(x: R) = toWhere().lessThanOrEqualTo(x)
-fun <T, R : Comparable<R>> WhereBuilder<T, R?>.lessThanOrEqualTo(x: R) = spec { lessThanOrEqualTo<R>(it, x) }
+fun <T, R : Comparable<R>> WhereBuilder<T, R?>.lessThanOrEqualTo(x: R, distinct: Boolean = false) = spec(distinct) { lessThanOrEqualTo<R>(it, x) }
 
 fun <T, R : Comparable<R>> KProperty1<in T, R?>.greaterThan(x: R) = toWhere().greaterThan(x)
-fun <T, R : Comparable<R>> WhereBuilder<T, R?>.greaterThan(x: R) = spec { greaterThan<R>(it, x) }
+fun <T, R : Comparable<R>> WhereBuilder<T, R?>.greaterThan(x: R, distinct: Boolean = false) = spec(distinct) { greaterThan<R>(it, x) }
 
 fun <T, R : Comparable<R>> KProperty1<in T, R?>.greaterThanOrEqualTo(x: R) = toWhere().greaterThanOrEqualTo(x)
-fun <T, R : Comparable<R>> WhereBuilder<T, R?>.greaterThanOrEqualTo(x: R) = spec { greaterThanOrEqualTo<R>(it, x) }
+fun <T, R : Comparable<R>> WhereBuilder<T, R?>.greaterThanOrEqualTo(x: R, distinct: Boolean = false) = spec(distinct) { greaterThanOrEqualTo<R>(it, x) }
 
 fun <T, R : Comparable<R>> KProperty1<in T, R?>.between(x: R, y: R) = toWhere().between(x, y)
-fun <T, R : Comparable<R>> WhereBuilder<T, R?>.between(x: R, y: R) = spec { between<R>(it, x, y) }
+fun <T, R : Comparable<R>> WhereBuilder<T, R?>.between(x: R, y: R, distinct: Boolean = false) = spec(distinct) { between<R>(it, x, y) }
 
 // True/False
 fun <T> KProperty1<in T, Boolean?>.isTrue() = toWhere().isTrue()
 
-fun <T> WhereBuilder<T, Boolean?>.isTrue() = spec { isTrue(it) }
+fun <T> WhereBuilder<T, Boolean?>.isTrue(distinct: Boolean = false) = spec(distinct) { isTrue(it) }
 
 fun <T> KProperty1<in T, Boolean?>.isFalse() = toWhere().isFalse()
-fun <T> WhereBuilder<T, Boolean?>.isFalse() = spec { isFalse(it) }
+fun <T> WhereBuilder<T, Boolean?>.isFalse(distinct: Boolean = false) = spec(distinct) { isFalse(it) }
 
 // Null / NotNull
 fun <T, R> KProperty1<in T, R?>.isNull() = toWhere().isNull()
 
-fun <T, R> WhereBuilder<T, R?>.isNull() = spec { isNull(it) }
+fun <T, R> WhereBuilder<T, R?>.isNull(distinct: Boolean = false) = spec(distinct) { isNull(it) }
 
 fun <T, R> KProperty1<in T, R?>.isNotNull() = toWhere().isNotNull()
-fun <T, R> WhereBuilder<T, R?>.isNotNull() = spec { isNotNull(it) }
+fun <T, R> WhereBuilder<T, R?>.isNotNull(distinct: Boolean = false) = spec(distinct) { isNotNull(it) }
 
 // Collections
 fun <T, R : Collection<*>> KProperty1<in T, R?>.isEmpty() = toWhere().isEmpty()
 
-fun <T, R : Collection<*>> WhereBuilder<T, R?>.isEmpty() = spec { isEmpty(it) }
+fun <T, R : Collection<*>> WhereBuilder<T, R?>.isEmpty(distinct: Boolean = false) = spec(distinct) { isEmpty(it) }
 
 fun <T, R : Collection<*>> KProperty1<in T, R?>.isNotEmpty() = toWhere().isNotEmpty()
-fun <T, R : Collection<*>> WhereBuilder<T, R?>.isNotEmpty() = spec { isNotEmpty(it) }
+fun <T, R : Collection<*>> WhereBuilder<T, R?>.isNotEmpty(distinct: Boolean = false) = spec(distinct) { isNotEmpty(it) }
 
 fun <T, E, R : Collection<E>> KProperty1<in T, R?>.isMember(elem: E) = toWhere().isMember(elem)
-fun <T, E, R : Collection<E>> WhereBuilder<T, R?>.isMember(elem: E) = spec { isMember(elem, it) }
+fun <T, E, R : Collection<E>> WhereBuilder<T, R?>.isMember(elem: E, distinct: Boolean = false) = spec(distinct) { isMember(elem, it) }
 
 fun <T, E, R : Collection<E>> KProperty1<in T, R?>.isNotMember(elem: E) = toWhere().isNotMember(elem)
-fun <T, E, R : Collection<E>> WhereBuilder<T, R?>.isNotMember(elem: E) = spec { isNotMember(elem, it) }
+fun <T, E, R : Collection<E>> WhereBuilder<T, R?>.isNotMember(elem: E, distinct: Boolean = false) = spec(distinct) { isNotMember(elem, it) }
 
 // Strings
 fun <T> KProperty1<in T, String?>.like(x: String): Specification<T> = toWhere().like(x)
 
-fun <T> WhereBuilder<T, String?>.like(x: String): Specification<T> = spec { like(it, x) }
+fun <T> WhereBuilder<T, String?>.like(x: String, distinct: Boolean = false): Specification<T> = spec(distinct) { like(it, x) }
 
 fun <T> KProperty1<in T, String?>.likeLower(x: String): Specification<T> = toWhere().likeLower(x)
-fun <T> WhereBuilder<T, String?>.likeLower(x: String): Specification<T> = spec { like(lower(it), x.toLowerCase()) }
+fun <T> WhereBuilder<T, String?>.likeLower(x: String, distinct: Boolean = false): Specification<T> = spec(distinct) { like(lower(it), x.toLowerCase()) }
 
 fun <T> KProperty1<in T, String?>.like(x: String, escapeChar: Char): Specification<T> = toWhere().like(x, escapeChar)
-fun <T> WhereBuilder<T, String?>.like(x: String, escapeChar: Char): Specification<T> = spec { like(it, x, escapeChar) }
+fun <T> WhereBuilder<T, String?>.like(x: String, escapeChar: Char, distinct: Boolean = false): Specification<T> = spec(distinct) { like(it, x, escapeChar) }
 
 fun <T> KProperty1<in T, String?>.notLike(x: String): Specification<T> = toWhere().notLike(x)
-fun <T> WhereBuilder<T, String?>.notLike(x: String): Specification<T> = spec { notLike(it, x) }
+fun <T> WhereBuilder<T, String?>.notLike(x: String, distinct: Boolean = false): Specification<T> = spec(distinct) { notLike(it, x) }
 
 fun <T> KProperty1<in T, String?>.notLike(x: String, escapeChar: Char): Specification<T> = toWhere().notLike(x, escapeChar)
-fun <T> WhereBuilder<T, String?>.notLike(x: String, escapeChar: Char): Specification<T> = spec { notLike(it, x, escapeChar) }
+fun <T> WhereBuilder<T, String?>.notLike(x: String, escapeChar: Char, distinct: Boolean = false): Specification<T> = spec(distinct) { notLike(it, x, escapeChar) }
 
 // And
 infix fun <T> Specification<T>?.and(other: Specification<in T>?): Specification<T> = and(listOf(this, other))
